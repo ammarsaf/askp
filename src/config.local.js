1cf3db93fb14f6a9936f2e938ba649e0c4d77169
ammar <muhammarsafuan@gmail.com>
2025-05-11 22:20:32 +0800

    fix: remove url

--- src/config.local.js
+++ src/config.local.js
@@ -2,7 +2,8 @@
 // This file should be modified by developers for their local environment
 // DO NOT commit this file to version control
 const localConfig = {
-    API_URL: 'http://localhost:8787/', // Change this to your local worker URL
+    //API_URL: 'http://localhost:8787/', // Change this to your local worker URL
 };
 
 export default localConfig; 
+//bla bla
