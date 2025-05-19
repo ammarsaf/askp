133bbecd0cedb2f42c910516e4a9dae71f0579e4
ammar <muhammarsafuan@gmail.com>
2025-05-19 21:46:09 +0800

    react rewrite initial commit

---
+++ mawaddah/components/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="flex flex-row justify-evenly">
+        <div>Mawaddah</div>
+        <div>About Us</div>
+        <div>Pricing</div>
+        <div>Login</div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ mawaddah/src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from '../components/Header';
+import 'tailwindcss';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <div>
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
